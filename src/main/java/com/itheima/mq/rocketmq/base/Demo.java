0973a6bedc9ce5d712779dae9f753397d92e3154
GGBond <aggbond@gmail.com>
2020-09-21 22:03:12 +0800

    rocketmq-demo init

---
+++ src/main/java/com/itheima/mq/rocketmq/base/Demo.java
@@ -0,0 +1,9 @@
+package com.itheima.mq.rocketmq.base;
+
+/**
+ * @author GG Bond
+ * @date 2020/4/17 19:52
+ * @description
+ */
+public class Demo {
+}
---
+++ src/main/java/com/itheima/mq/rocketmq/base/producer/SyncProducer.java
@@ -0,0 +1,9 @@
+package com.itheima.mq.rocketmq.base.producer;
+
+/**
+ * @author GG Bond
+ * @date 2020/9/21 21:58
+ * @description 发送同步消息
+ */
+public class SyncProducer {
+}
